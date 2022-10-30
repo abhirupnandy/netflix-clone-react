8dd96047744c1b89f65084956e1dd9010eaec1de
Abhirup Nandy <abhirupnandy.rocks@gmail.com>
2022-10-31 00:57:17 +0530

    protected-route-fix

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-	const { user } = UserAuth();
-	if (!user) {
-		return <Navigate to='/' />;
-	} else {
-		return children;
-	}
+	const { authed } = UserAuth();
+	var navigate = useNavigate();
+
+	return authed === true ? children : navigate('/');
 };
 
 export default ProtectedRoute;
